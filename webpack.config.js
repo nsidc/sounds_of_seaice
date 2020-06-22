e2310c5f4c8d8878c833d94a9a13c291cbc7c1b9
Trey Stafford <trey.stafford@nsidc.org>
2020-06-22 09:51:08 -0600

    Add and use `{html,clean}-webpack-plugin`

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +9,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Sounds of Sea Ice',
+    }),
+  ],
 };
