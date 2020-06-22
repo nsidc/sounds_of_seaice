6feaf892b5647ca414688b21ebf803be1d7c6c2d
Trey Stafford <trey.stafford@nsidc.org>
2020-06-22 17:11:15 -0600

    Fetch northern hemisphere extent data from `seaiceservice`

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 // https://github.com/Tonejs/Tone.js/#hello-tone
 import { Sequence, Synth, Transport } from "tone";
 
-import { example_data } from './constants.js';
-
-// Start the clock
-Transport.toggle();
+import { fetch_url} from './constants';
 
 // create a synth and connect it to the master output
 const synth = new Synth().toMaster();
@@ -14,18 +11,33 @@ const normalize = (data, new_min, new_max) => {
   const min = Math.min.apply(Math, values);
   const max = Math.max.apply(Math, values);
 
-  const newValues = values.map((value) => {
-    return (new_max - new_min) * ((value - min) / (max - min)) + new_min;
-  });
+  // TODO sort by date (key of data object)
+  let newValues = [];
+  for (let [date, value] of Object.entries(data)) {
+    newValues.push({
+      date,
+      value: (new_max - new_min) * ((value - min) / (max - min)) + new_min});
+  }
 
   return newValues;
 };
 
-const normalized_values = normalize(example_data, 50, 1000);
+const initialize_sequence_with_data = () => {
+  fetch(fetch_url)
+  .then(response => response.json())
+  .then(data => {
+    // TODO: display date along w/ tone.
+    const normalized_values = normalize(data, 50, 1000);
 
-const seaice_seq = new Sequence((time, note) => {
-  synth.triggerAttackRelease(note, '16n', time);
-}, normalized_values, '16n');
+    const seaice_seq = new Sequence((time, note) => {
+      console.log(note.date);
+      synth.triggerAttackRelease(note.value, '16n', time);
+    }, normalized_values, '16n');
 
-seaice_seq.loop = false;
-seaice_seq.start();
+    seaice_seq.loop = false;
+    Transport.start();
+    seaice_seq.start();
+  });
+};
+
+initialize_sequence_with_data();
