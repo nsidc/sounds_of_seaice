221be305ae37cd61297d2f9c76d1a4876ff267ae
Trey Stafford <trey.stafford@nsidc.org>
2020-06-23 17:27:25 -0600

    For now, use monthly data instead of daily
    Might still be worth using daily data in the future.

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
-// TODO: parameterize hemisphere, start, & end date.
-export const fetch_url = 'https://nsidc.org/api/seaiceservice/extent/north/data/?start_date=2012-01-01&end_date=2013-01-01'
+// TODO: parameterize hemisphere, start, & end date, temporality(?)
+
+// daily
+// export const fetch_url = 'https://nsidc.org/api/seaiceservice/extent/north/data/?start_date=2012-01-01&end_date=2013-01-01';
+
+// monthly
+export const fetch_url = 'https://nsidc.org/api/seaiceservice/monthly/extent/north/data/';
