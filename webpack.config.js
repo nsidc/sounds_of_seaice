a8c0ef15c6b179f4fdc736566960ec26af869957
Trey Stafford <trey.stafford@nsidc.org>
2020-07-29 13:36:31 -0600

    webpack config: default to 'dev' settings.
    Use production settings when `env.production` is set and `true`.

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,32 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = env => {
+  if (env.hasOwnProperty('production') && env.production) {
+    env.NODE_ENV = 'production';
+  } else {
+    env.NODE_ENV = 'development';
+  }
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Sounds of Sea Ice',
-      template: path.resolve(__dirname, 'src', 'index.ejs'),
-    }),
-  ],
+  console.log('Using', env.NODE_ENV, 'settings.');
+
+  return {
+    mode: env.NODE_ENV,
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Sounds of Sea Ice',
+        template: path.resolve(__dirname, 'src', 'index.ejs'),
+      }),
+    ],
+  };
 };
